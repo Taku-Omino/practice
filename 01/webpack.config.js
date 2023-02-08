e52c4bcb8f9548658d375417e0933bad76a3606d
Taku-Omino <47404544+Taku-Omino@users.noreply.github.com>
2023-02-08 10:32:14 +0900

    Add files via upload

---
+++ 01/webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+      template: './src/index.ejs',
+    }),
+    //new HtmlWebpackPlugin({
+      // ここにaboutのパスを書く
+      // aboutディレクトリを作成しそのディレクトリ内のindex.ejsに記述すること
+    //}),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ejs$/,
+        use: 'ejs-compiled-loader',
+      },
+    ],
+  },
+  devServer: {
+    compress: true,
+    port: 9000,
+  },
+}
+
