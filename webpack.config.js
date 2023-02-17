45cf8b4cda1a5eff66784ac34bcbe173abc03953
Taku-Omino <47404544+Taku-Omino@users.noreply.github.com>
2023-02-17 11:42:28 +0900

    1-EJS/02 First commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "./index.html",
+      template: "./src/index.ejs",
+    }),
+    //new HtmlWebpackPlugin({
+    // ここにaboutのパスを書く
+    // aboutディレクトリを作成しそのディレクトリ内のindex.ejsに記述すること
+    //}),
+    new HtmlWebpackPlugin({
+      filename: "./about.html",
+      template: "./src/about.ejs",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ejs$/,
+        loader: "ejs-compiled-loader",
+        options: {
+          htmlmin: true,
+          htmlminOptions: {
+            removeComments: true,
+          },
+          compiler: require("ejs"),
+          compilerOptions: {
+            client: true,
+          },
+          htmlWebpackPlugin: {
+            files: {
+              template: "src/templates/_head.ejs",
+            },
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    compress: true,
+    port: 9000,
+  },
+};
