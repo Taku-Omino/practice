ac3b1ec5ce7e0d63149829617220228c8f0cc4d3
Taku-Omino <47404544+Taku-Omino@users.noreply.github.com>
2023-02-18 18:52:30 +0900

    change "include" into "require"

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,11 @@ module.exports = {
           compilerOptions: {
             client: true,
           },
+          htmlWebpackPlugin: {
+            files: {
+              template: "src/templates/_head.ejs",
+            },
+          },
         },
       },
     ],
