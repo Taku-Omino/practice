3e012a9dda274a347d5e01c0bd67da83645778ca
Taku-Omino <47404544+Taku-Omino@users.noreply.github.com>
2023-03-08 07:47:07 +0900

    database

---
+++ database/seeders/CompaniesTableSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // 追加
+
+class CompaniesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+public function run()
+{
+    DB::table('companies')->insert([
+        [
+            'user_name' => 'Steve Jobs',
+            'workplace' => 'Apple'
+        ],
+        [
+            'user_name' => 'Bill Gates',
+            'workplace' => 'Microsoft'
+        ],
+        [
+            'user_name' => 'Mark Zuckerberg',
+            'workplace' => 'Facebook'
+        ],
+        [
+            'user_name' => 'Jeff Bezos',
+            'workplace' => 'Amazon'
+        ],
+        [
+            'user_name' => 'Larry Page',
+            'workplace' => 'Google'
+        ],
+        [
+            'user_name' => 'Sergey Brin',
+            'workplace' => 'Google'
+        ],
+        [
+            'user_name' => 'Elon Musk',
+            'workplace' => 'Tesla'
+        ],
+        [
+            'user_name' => 'Jack Ma',
+            'workplace' => 'Alibaba'
+        ],
+        [
+            'user_name' => 'Masayoshi Son',
+            'workplace' => 'Softbank'
+        ],
+        [
+            'user_name' => 'Hiroshi Mikitani',
+            'workplace' => 'Rakuten'
+        ]
+    ]);
+}
+
+}
